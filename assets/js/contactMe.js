83f50025e4f2f7e061d679c53efc095992cf21d6
thedoorman <jesse.clarkd@gmail.com>
2017-11-22 10:19:08 -0500

    Options + removed inline script

--- assets/js/contactMe.js
+++ assets/js/contactMe.js
@@ -6,6 +6,20 @@ function corsPost() {
   "async": true,
   "crossDomain": true,
   "url": "https://ttzisvgb7j.execute-api.us-east-1.amazonaws.com/prod/contactMe?name="+name+"&email="+email+"&message="+message,
+  "method": "OPTIONS",
+  "headers": {
+    "access-control-allow-origin": "*",
+    "access-control-allowheaders": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+  }
+}
+console.log("Settings: " + settings)
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://ttzisvgb7j.execute-api.us-east-1.amazonaws.com/prod/contactMe?name="+name+"&email="+email+"&message="+message,
   "method": "POST",
   "headers": {
     "access-control-allow-origin": "*",
