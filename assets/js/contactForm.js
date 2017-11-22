44af927dc4116b346a918f26a8c747142d4a55da
thedoorman <jesse.clarkd@gmail.com>
2017-11-22 14:47:49 -0500

    Mirroring what worked

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -4,37 +4,41 @@ var createCORSRequest = function(method, url) {
 
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
-      // Most browsers.
-      xhr.open(method, url, true);
+        // Most browsers.
+        xhr.open(method, url, true);
     } else if (typeof XDomainRequest != "undefined") {
-      // IE8 & IE9
-      xhr = new XDomainRequest();
-      xhr.open(method, url);
+        // IE8 & IE9
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
     } else {
-      // CORS not supported.
-      xhr = null;
+        // CORS not supported.
+        xhr = null;
     }
     return xhr;
-  };
+    };
  
-console.log('INFO: Grabbing parameters!')    
-var name = document.getElementsByName("name")[0].value;
-var email = document.getElementsByName("email")[0].value;
-var message = document.getElementsByName("message")[0].value;
-var url = 'https://ttzisvgb7j.execute-api.us-east-1.amazonaws.com/prod/contactMe?name='+name+'&email='+email+'&message='+message;
-var method = 'POST';
-console.log('INFO: URL: '+ url)        
-var xhr = createCORSRequest(method, url);
-xhr.onload = function() {
-    // Success code goes here.
-    console.log('SUCCESS: Made the call!')
-    };
-    
-    xhr.onerror = function() {
-    // Error code goes here.
-    console.log('ERROR: Did not make the call!')
-    
-    };
-    
-    xhr.send();
+    console.log('INFO: Grabbing parameters!');
+        
+    var name = document.getElementsByName("name")[0].value;
+    var email = document.getElementsByName("email")[0].value;
+    var message = document.getElementsByName("message")[0].value;
+
+    console.log('INFO: URL: '+ url);
+
+    var url = 'https://ttzisvgb7j.execute-api.us-east-1.amazonaws.com/prod/contactMe?name='+name+'&email='+email+'&message='+message;
+    var method = 'POST';
+    var xhr = createCORSRequest(method, url);
+
+    xhr.onload = function() {
+        // Success code goes here.
+        console.log('SUCCESS: Made the call!')
+        };
+        
+        xhr.onerror = function() {
+        // Error code goes here.
+        console.log('ERROR: Did not make the call!')
+        
+        };
+        
+        xhr.send();
   
