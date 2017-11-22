d4bd013d062b7bfa7088f31ef6622dca0b995603
thedoorman <jesse.clarkd@gmail.com>
2017-11-22 12:02:25 -0500

    switched $.post with $.ajax

--- assets/js/contactMe.js
+++ assets/js/contactMe.js
@@ -19,7 +19,7 @@ console.log("Settings: " +
 " method: " + settings.method +
 " headers: " + settings.headers);
 
-$.post(settings).done(function (response) {
+$.ajax(settings).done(function (response) {
   console.log(response);
 });
 
