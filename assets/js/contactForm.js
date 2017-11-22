9a25d897647bae451b1b6f668b727b4e41364f93
thedoorman <jesse.clarkd@gmail.com>
2017-11-22 14:40:58 -0500

    added print statements to debug

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -1,3 +1,4 @@
+console.log('INFO: Setting the createCorsRequest!')
 var createCORSRequest = function(method, url) {
     console.log("INFO: Sending POST request");
 
@@ -17,18 +18,23 @@ var createCORSRequest = function(method, url) {
   };
  
 function makeCall(){
+    console.log('INFO: Grabbing parameters!')    
     var name = document.getElementsByName("name")[0].value;
     var email = document.getElementsByName("email")[0].value;
     var message = document.getElementsByName("message")[0].value;
     var url = 'https://ttzisvgb7j.execute-api.us-east-1.amazonaws.com/prod/contactMe?name='+name+'&email='+email+'&message='+message;
     var method = 'POST';
+    console.log('INFO: URL: '+ url)        
     var xhr = createCORSRequest(method, url);
     xhr.onload = function() {
         // Success code goes here.
+        console.log('SUCCESS: Made the call!')
         };
         
         xhr.onerror = function() {
         // Error code goes here.
+        console.log('ERROR: Did not make the call!')
+        
         };
         
         xhr.send();
