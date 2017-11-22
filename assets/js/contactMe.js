44d4355f0ea8c64d9ed3addc2d33412bf6aec046
thedoorman <jesse.clarkd@gmail.com>
2017-11-22 12:10:36 -0500

    refactored 'settings'

--- assets/js/contactMe.js
+++ assets/js/contactMe.js
@@ -4,12 +4,11 @@ function corsPost() {
   var email = document.getElementsByName("email")[0].value;
   var message = document.getElementsByName("message")[0].value;
   var settings = {
-  "async": true,
+  "headers": {"Accept": "application/json"},
   "crossDomain": true,
   "url": "https://ttzisvgb7j.execute-api.us-east-1.amazonaws.com/prod/contactMe?name="+name+"&email="+email+"&message="+message,
   "data": "",
   "method": "POST",
-  "headers": {}
 }
 console.log("Settings: " + 
 " Async: " + settings.async +
