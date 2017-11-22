fbbbc6d30c88244cfb716c88558badb5432ce13c
thedoorman <jesse.clarkd@gmail.com>
2017-11-22 10:13:47 -0500

    Added cors headers

--- assets/js/contactMe.js
+++ assets/js/contactMe.js
@@ -1,4 +1,4 @@
-function post() {
+function corsPost() {
   var name = document.getElementsByName("name")[0].value;
   var email = document.getElementsByName("email")[0].value;
   var message = document.getElementsByName("message")[0].value;
@@ -7,7 +7,10 @@ function post() {
   "crossDomain": true,
   "url": "https://ttzisvgb7j.execute-api.us-east-1.amazonaws.com/prod/contactMe?name="+name+"&email="+email+"&message="+message,
   "method": "POST",
-  "headers": {}
+  "headers": {
+    "access-control-allow-origin": "*",
+    "access-control-allowheaders": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+  }
 }
 console.log("Settings: " + settings)
 $.ajax(settings).done(function (response) {
