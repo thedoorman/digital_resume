d3e6e39b265e0e1396c69845d96d0fcb16f08b47
thedoorman <jesse.clarkd@gmail.com>
2017-11-22 14:05:23 -0500

    changed to .val for getting html vals

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -1,8 +1,8 @@
 var createCORSRequest = function(method, url) {
     console.log("INFO: Sending POST request");
-    var name = document.getElementsByName("name").val();
-    var email = document.getElementsByName("email").val();
-    var message = document.getElementsByName("message").val();
+    var name = document.getElementsByName("name").val;
+    var email = document.getElementsByName("email").val;
+    var message = document.getElementsByName("message").val;
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
       // Most browsers.
