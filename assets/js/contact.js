8e010bd6eaf370d1358d5f43982856627851cf85
thedoorman <jesse.clarkd@gmail.com>
2017-12-07 16:45:59 -0500

    Updated node to use promise

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,29 +1,17 @@
-var http = require("https");
-
 function postReq(){
-    var options = {
-    "method": "POST",
-    "hostname": "ttzisvgb7j.execute-api.us-east-1.amazonaws.com",
-    "port": null,
-    "path": "/prod/contactMe?name=Jesse%20Clark&email=yourmom%40gmail.com&message=How%20are%20you%20doing%20you%20idiot!%3F",
-    "headers": {
-        "cache-control": "no-cache",
-        "postman-token": "2bb56b25-b20d-55fd-769b-09dbe4431ba3"
+    var name = document.getElementsByName("name")[0].value;
+    var email = document.getElementsByName("email")[0].value;
+    var message = document.getElementsByName("message")[0].value;
+    let promise = new Promise((resolve, reject) => {
+        let apiURL = `https://api.jesseclark.io/contactMe?name=${name}&email=${email}&message=${message}`;
+        this.http.get(apiURL)
+          .toPromise()
+          .then(
+            res => { // Success
+              console.log(res.json());
+              resolve();
+            }
+          );
+      });
+      return promise;
     }
-    };
-
-    var req = http.request(options, function (res) {
-    var chunks = [];
-
-    res.on("data", function (chunk) {
-        chunks.push(chunk);
-    });
-
-    res.on("end", function () {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-    });
-    });
-
-    req.end();
-}
